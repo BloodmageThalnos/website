0c0a07f7dd5b7816ee1cdff1c08223d7f5f7f74f
Cheng Yuan <cy1818cy@bupt.edu.cn>
2019-03-14 13:55:04 +0800

    update mainApp uploading Article

--- mainApp/models.py
+++ mainApp/models.py
@@ -7,14 +7,19 @@ class ArticleModel(models.Model):
     title = models.TextField()
     content = models.TextField()
     create_time = models.TimeField(auto_now=True)
-    author_id = models.IntegerField(default=0)
+    author_id = models.IntegerField()
+    cover_img = models.CharField(max_length=64)
+
     # comments
     # visited
 
 def getRecentArticles_and_cache(num):
     articles = ArticleModel.objects.order_by('-create_time')[:num]
-    cache.set('recent_articles', articles.query)
-    return articles
+    cache.set('recent_articles_'+str(num), articles.query)
+
+# def saveArticle(title, content, author_id=0):
+#    am = ArticleModel(title=title,content=content,author_id=author_id)
+#    am.save()
 
 class CommentModel(models.Model):
     pass
