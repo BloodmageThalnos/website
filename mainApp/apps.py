1b166c981b9b73ae6a6ade974f68c5afbc63311f
Cheng Yuan <cy1818cy@bupt.edu.cn>
2019-05-24 08:36:27 +0800

    Version 1.03.0002 add Ready logic

--- mainApp/apps.py
+++ mainApp/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
+import os
 
 
 class MainappConfig(AppConfig):
     name = 'mainApp'
+    def ready(self):
+        os.makedirs('./music', exist_ok=True)
+        os.makedirs('./log', exist_ok=True)
+        os.makedirs('./life', exist_ok=True)
