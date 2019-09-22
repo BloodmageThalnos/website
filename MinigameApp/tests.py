d9e38cd21d678e14a10ca522dc9e705257c7576d
Cheng Yuan <cy1818cy@bupt.edu.cn>
2019-09-22 20:33:46 +0800

    update cv.html

---
+++ MinigameApp/tests.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+
+class modelTestCase(TestCase):
+    def setUp(self):
+        pass
+
+    def test_1(self):
+        pass
